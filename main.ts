70e0a0a2f81b570b8d779a88ef8f02e6067eee66
harryszeto6 <92395104+harryszeto6@users.noreply.github.com>
2022-01-20 14:17:49 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let mySprite = sprites.create(img`
+let mallll = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
