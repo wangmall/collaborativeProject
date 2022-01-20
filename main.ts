048b7965a9b5ef075c14852024316dfb0803e743
wangmall <98120950+wangmall@users.noreply.github.com>
2022-01-20 14:30:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 let monkey = sprites.create(img`
     . . . . . . . f f f f f . . . . 
     . . . . . . f e e e e e f . . . 
-    . . . . . f e e e d d d d f . . 
-    . . . . f f e e d f d d f d c . 
-    . . . f d d e e d f d d f d c . 
-    . . . c d b e e d d d d e e d c 
-    f f . c d b e e d d c d d d d c 
-    f e f . c f e e d d d c c c c c 
-    f e f . . f f e e d d d d d f . 
+    . . . . f f e e e d d d d f . . 
+    . . . f d d e e d d d d d d c . 
+    . . . c d b e e d f d d f d c . 
+    f f . c d b e e d f d d f d d c 
+    f e f . c f e e d d d d e e d c 
+    f e f . . f e e e d c d d d d c 
+    f e f . . f f e e e d c c c f . 
     f e f . f e e e e f f f f f . . 
-    f e f f e e e e e e e f . . . . 
-    . f f e e e e f e f f e f . . . 
-    . . f e e e e f e f f e f . . . 
-    . . . f e f f b d f b d f . . . 
-    . . . f d b b d d c d d f . . . 
-    . . . f f f f f f f f f . . . . 
+    . f f f e e e e e e e f . . . . 
+    . . f e e e e f e e f e f f . . 
+    . . f e e e f f f e e f f e f . 
+    . f b f f f f f f c d d b d d f 
+    . f d d c f . . f d d d c d d f 
+    . . f f f . . . f f f f f f f . 
     `, SpriteKind.Player)
